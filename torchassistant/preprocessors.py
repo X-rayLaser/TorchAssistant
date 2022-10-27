fc6b9e7b75ddc4393b2a9d1434daa57b0c4d301a
Eugene <supernovaprotocol@gmail.com>
2022-10-27 14:19:52 +0700

    Refactor image preprocessor

--- torchassistant/preprocessors.py
+++ torchassistant/preprocessors.py
@@ -1,6 +1,8 @@
+from random import shuffle
 import torch
 
 from torchvision.transforms import ToTensor
+from torchassistant.formatters import ProgressBar
 
 
 class ValuePreprocessor:
@@ -37,30 +39,60 @@ class NullProcessor(ValuePreprocessor):
 
 class SimpleNormalizer(ValuePreprocessor):
     def process(self, value):
-        print('yes simple preprocessor')
         return value / 255
 
 
-class ImagePreprocessor(ValuePreprocessor):
+class TrainablePreprocessor(ValuePreprocessor):
+    def fit(self, dataset):
+        progress_bar = ProgressBar()
+
+        indices = self._try_shuffling_indices(dataset)
+
+        total_steps = int(len(dataset) * 0.1)
+        indices = indices[:total_steps]
+        gen = self._do_fit(dataset, indices)
+        for i, idx in enumerate(gen):
+            if (i + 1) % 100 == 0:
+                step_number = i + 1
+                progress = progress_bar.updated(step_number, total_steps, cols=50)
+                print(f'\rFitting preprocessor: {progress} {step_number}/{total_steps}', end='')
+
+        whitespaces = ' ' * 100
+        print(f'\r{whitespaces}\rDone!')
+
+    def _try_shuffling_indices(self, dataset):
+        num_examples = len(dataset)
+        if num_examples < 10 ** 7:
+            indices = list(range(num_examples))
+            shuffle(indices)
+        else:
+            indices = range(num_examples)
+        return indices
+
+    def _do_fit(self, dataset, indices):
+        """A generator object that performs fitting and yields index of currently fit example"""
+        yield 0
+
+
+class ImagePreprocessor(TrainablePreprocessor):
     def __init__(self, fit_index):
         self.fit_index = fit_index
         self.mu = []
         self.sd = []
 
-    def fit(self, dataset):
+    def _do_fit(self, dataset, indices):
         to_tensor = ToTensor()
 
         mu = []
         sd = []
-        # todo: shuffle indices and use 1/10 first ones to fit
-        for i, example in enumerate(dataset):
-            if i > 1000:
-                break
 
+        for idx in indices:
+            example = dataset[idx]
             pil_image = example[self.fit_index]
             tensor = to_tensor(pil_image)
             mu.append(tensor.mean(dim=[1, 2]))  # channel-wise statistics
             sd.append(tensor.std(dim=[1, 2]))
+            yield idx
 
         # todo: maybe find sd in terms of found mu in its formula
         self.mu = torch.stack(mu).mean(dim=0).tolist()
@@ -72,6 +104,7 @@ class ImagePreprocessor(ValuePreprocessor):
         num_channels = len(tensor)
         for c in range(num_channels):
             tensor[c] = (tensor[c] - self.mu[c]) / self.sd[c]
+
         return tensor
 
     def state_dict(self):
